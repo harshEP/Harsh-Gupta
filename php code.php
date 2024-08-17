965371f21894a268120ec7ff4b3842a754b97ef5
Harsh Gupta <103250405+harshEP@users.noreply.github.com>
2024-08-17 15:04:12 +0530

    Long-terminvesting
    Long-term Investing is my own business website which was made by me and my friend for my investing and consulting business as providing financial education and advisory services. Technology used in this website is HTML, CSS, Java Script, PHP, Word press, React.

---
+++ php code.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title> php code </title>
+    </head>
+    <body>
+        
+        <?php
+        echo "<h2> this is my 1st php code </h2>";
+
+        ?>
+
+    </body>
+</html>
